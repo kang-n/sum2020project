ab5db5a674f192d2657be461a73120afff3ecdbd
Nina K <kyanan08@gmail.com>
2020-06-12 17:12:46 -0500

    added sportbot

---
+++ sportbot/main.py
@@ -0,0 +1,17 @@
+import discord
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+
+client.run(TOKEN)
