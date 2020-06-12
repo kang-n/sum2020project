c522da2a1ac13fd5b56611808c4ac2ae759d8148
Nina K <kyanan08@gmail.com>
2020-06-12 18:26:12 -0500

    reverted change

--- sportbot/main.py
+++ sportbot/main.py
@@ -11,7 +11,6 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    await send("Thanks for inviting sportbot to your server")
 
 @client.event
 async def on_message(message):
